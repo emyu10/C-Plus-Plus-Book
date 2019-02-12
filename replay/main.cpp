d4709ba216e43208411a86e2e030dcb260995150
Mohamed Usman <emyu10@gmail.com>
2019-02-12 12:05:15 +0500

    an int array example

---
+++ replay/main.cpp
@@ -0,0 +1,19 @@
+// replay
+// gets four ages from user, displays them
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age[4];
+    for (int j = 0; j < 4; j++) {
+        cout << "Enter an age: ";
+        cin >> age[j];
+    }
+
+    for (int j = 0; j < 4; j++) {
+        cout << "You entered " << age[j] << endl;
+    }
+    return 0;
+}
